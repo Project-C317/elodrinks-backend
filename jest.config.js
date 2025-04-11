012347192fa8f1c4f1a3fc100ab880aae8bfe75c
Iza Lopes <izalopes872@outlook.com>
2025-04-10 21:27:33 -0300

    test(config): adiciona configuração do jest para testes e cobertura

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!**/node_modules/**',
+  ],
 };
