1ee4eb25ee810f0d8fe5d17ee98a4657075c391a
Iza Lopes <izalopes872@outlook.com>
2025-04-16 08:44:26 -0300

    refactor(database): mover lógica de conexão com MongoDB para classe separada

--- src/database/DataConnection.ts
+++ src/database/DataConnection.ts
@@ -1,5 +1,19 @@
+import mongoose from 'mongoose';
+
 export class DatabaseConnection {
   async connect(): Promise<void> {
-    console.log('Banco de dados conectado com sucesso!');
+    const mongoURI = process.env.MONGO_URI as string;
+
+    if (!mongoURI) {
+      throw new Error('MONGO_URI não está definido no arquivo .env');
+    }
+
+    try {
+      await mongoose.connect(mongoURI);
+      console.info('Conexão com MongoDB realizada com sucesso');
+    } catch (error) {
+      console.error('Erro ao conectar no MongoDB:', error);
+      throw error;
+    }
   }
 }
