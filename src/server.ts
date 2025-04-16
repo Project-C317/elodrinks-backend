02ef4147e923b1331a43f9152fb8ad8a209d9bab
Iza Lopes <izalopes872@outlook.com>
2025-04-16 08:45:42 -0300

    refactor(server): usar classe DatabaseConnection para conexão com MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
+import { config } from 'dotenv';
+config();
+
 import { App } from './app';
+import { DatabaseConnection } from './database/DataConnection';
 
 async function bootstrap() {
   const PORT = parseInt(process.env.PORT || '3000', 10);
-  const app = new App(PORT);
 
+  const database = new DatabaseConnection();
+  await database.connect();
+
+  const app = new App(PORT);
   app.listen();
 }
 
