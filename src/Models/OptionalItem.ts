85b478fcaa5670e275070a3da2ef35cce4227876
Iza Lopes <izalopes872@outlook.com>
2025-05-06 20:10:55 -0300

    refactor: remover campo Id do schema

--- src/Models/OptionalItem.ts
+++ src/Models/OptionalItem.ts
@@ -1,7 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IOptionalItem extends Document {
-  Id: string;
   Name: string;
   PricePerUnit: number;
   Quantity: number;
@@ -9,7 +8,6 @@ export interface IOptionalItem extends Document {
 }
 
 export const OptionalItemSchema: Schema = new Schema<IOptionalItem>({
-  Id: { type: String, required: true },
   Name: { type: String, required: true },
   PricePerUnit: { type: Number, required: true },
   Quantity: { type: Number, required: true },
