c6485bdc70284669f831c2ab36e7ea96cc1d4530
Iza Lopes <izalopes872@outlook.com>
2025-05-13 10:02:16 -0300

    feat: adiciona autenticacao e autorizacao nas rotas de usuarios

--- src/Routes/UserRoutes.ts
+++ src/Routes/UserRoutes.ts
@@ -1,24 +1,21 @@
 import { Router } from 'express';
 import UserController from '../Controllers/UserController';
 import UserService from '../Services/UserService';
+import { authenticateToken, authorizeRole } from '../Middleware/authenticateToken';
 
 const userRouter = Router();
 
-// Initialize the controller with its service
 const userService = new UserService();
 const userController = new UserController(userService);
 
-// Define the routes and map them to the controller methods
-userRouter.get('/', (req, res) => userController.getAllUsers(req, res)); // GET /users
-userRouter.post('/', (req, res) => userController.registerUser(req, res)); // POST /users (Create User)
-
-// Authentication Routes
+// Rotas públicas (não precisa autenticar)
 userRouter.post('/register', (req, res) => userController.registerUser(req, res)); // POST /users/register (Register User)
 userRouter.post('/login', (req, res) => userController.loginUser(req, res)); // POST /users/login (Login User)
 
-// Other User Routes
-userRouter.get('/:id', (req, res) => userController.getUserById(req, res)); // GET /users/:id
-userRouter.put('/:id', (req, res) => userController.updateUserById(req, res)); // PUT /users/:id
-userRouter.delete('/:id', (req, res) => userController.deleteUserById(req, res)); // DELETE /users/:id
+// Rotas privadas (precisa autenticar)
+userRouter.get('/', authenticateToken, authorizeRole(['admin']), (req, res) => userController.getAllUsers(req, res)); // GET /users
+userRouter.get('/:id', authenticateToken, (req, res) => userController.getUserById(req, res)); // GET /users/:id
+userRouter.put('/:id', authenticateToken, (req, res) => userController.updateUserById(req, res)); // PUT /users/:id
+userRouter.delete('/:id', authenticateToken, authorizeRole(['admin']), (req, res) => userController.deleteUserById(req, res)); // DELETE /users/:id
 
 export default userRouter;
