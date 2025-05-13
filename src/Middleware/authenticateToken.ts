54d2b5023320ff545c7734fad2a834920d64c2a0
Iza Lopes <izalopes872@outlook.com>
2025-05-13 09:57:30 -0300

    feat: adiciona authorizeRole para controle de acesso por perfil de usuario

--- src/Middleware/authenticateToken.ts
+++ src/Middleware/authenticateToken.ts
@@ -1,7 +1,7 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = process.env.SECRET_KEY || "your_secret_key";
+const SECRET_KEY = process.env.SECRET_KEY || 'your_secret_key';
 
 export interface AuthenticatedRequest extends Request {
   user?: any; // Attach user data to the request
@@ -10,21 +10,37 @@ export interface AuthenticatedRequest extends Request {
 export const authenticateToken = (
   req: AuthenticatedRequest,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // Extract token
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1]; // Extract token
 
   if (!token) {
-    return res.status(401).json({ message: "No token provided" });
+    return res.status(401).json({ message: 'No token provided' });
   }
 
   jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
-      return res.status(403).json({ message: "Invalid or expired token" });
+      return res.status(403).json({ message: 'Invalid or expired token' });
     }
 
     req.user = user; // Attach the decoded user data to the request object
     next(); // Proceed to the next middleware or route handler
   });
+};
+
+export const authorizeRole = (roles: string[]) => {
+  return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      return res.status(401).json({ message: 'Usuário não autenticado.' });
+    }
+    
+    if (roles.length && !roles.includes(req.user.role)) {
+      return res.status(403).json({ 
+        message: 'Acesso negado. Você não tem permissão para acessar este recurso.', 
+      });
+    }
+    
+    next();
+  };
 };
