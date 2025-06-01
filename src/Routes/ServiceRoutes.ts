676f32a6087d4638361d1fec83fb692b4a725a01
Iza Lopes <izalopes872@outlook.com>
2025-06-01 13:50:56 -0300

    refactor(services): define permissões para leitura (admin, user) e para modificações (admin)

--- src/Routes/ServiceRoutes.ts
+++ src/Routes/ServiceRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import ServiceController from '../Controllers/ServiceController';
 import ServiceService from '../Services/ServiceService';
-import { authenticateToken } from '../Middleware/authenticateToken';
+import { authenticateToken, authorizeRole } from '../Middleware/authenticateToken';
 
 const serviceRouter = Router();
 
@@ -9,10 +9,10 @@ const serviceService = new ServiceService();
 const serviceController = new ServiceController(serviceService);
 
 // Rotas privadas (precisa autenticar)
-serviceRouter.get('/', authenticateToken, (req, res) => serviceController.getAllServices(req, res)); // GET /services
-serviceRouter.post('/', authenticateToken, (req, res) => serviceController.createService(req, res)); // POST /services
-serviceRouter.get('/:id', authenticateToken, (req, res) => serviceController.getServiceById(req, res)); // GET /services/:id
-serviceRouter.put('/:id', authenticateToken, (req, res) => serviceController.updateServiceById(req, res)); // PUT /services/:id
-serviceRouter.delete('/:id', authenticateToken, (req, res) => serviceController.deleteServiceById(req, res)); // DELETE /services/:id
+serviceRouter.get('/', authenticateToken, authorizeRole(['admin', 'user']), (req, res) => serviceController.getAllServices(req, res)); // GET /services
+serviceRouter.post('/', authenticateToken, authorizeRole(['admin']), (req, res) => serviceController.createService(req, res)); // POST /services
+serviceRouter.get('/:id', authenticateToken, authorizeRole(['admin', 'user']), (req, res) => serviceController.getServiceById(req, res)); // GET /services/:id
+serviceRouter.put('/:id', authenticateToken, authorizeRole(['admin']), (req, res) => serviceController.updateServiceById(req, res)); // PUT /services/:id
+serviceRouter.delete('/:id', authenticateToken, authorizeRole(['admin']), (req, res) => serviceController.deleteServiceById(req, res)); // DELETE /services/:id
 
 export default serviceRouter;
