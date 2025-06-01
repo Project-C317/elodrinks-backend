1c3a4afe9eb4939372c859d4869191293d76ec4b
Iza Lopes <izalopes872@outlook.com>
2025-06-01 13:54:31 -0300

    refactor(optional-items): define permissões para leitura (admin, user) e para modificações (admin)

--- src/Routes/OptionalRoutes.ts
+++ src/Routes/OptionalRoutes.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 import OptionalController from '../Controllers/OptionalController';
 import OptionalService from '../Services/OptionalService';
-import { authenticateToken } from '../Middleware/authenticateToken';
+import { authenticateToken, authorizeRole } from '../Middleware/authenticateToken';
 
 const OptionalRouter = Router();
 const optionalService = new OptionalService();
 const optionalController = new OptionalController(optionalService);
 
 // Rotas privadas (precisa autenticar)
-OptionalRouter.get('/', authenticateToken, (req, res) => optionalController.getAllOptionalItems(req, res)); // GET /optional-items
-OptionalRouter.post('/', authenticateToken, (req, res) => optionalController.createOptionalItem(req, res)); // POST /optional-items
-OptionalRouter.get('/:id', authenticateToken, (req, res) => optionalController.getOptionalItemById(req, res)); // GET /optional-items/:id
-OptionalRouter.put('/:id', authenticateToken, (req, res) => optionalController.updateOptionalItemById(req, res)); // PUT /optional-items/:id
-OptionalRouter.delete('/:id', authenticateToken, (req, res) => optionalController.deleteOptionalItemById(req, res)); // DELETE /optional-items/:id
+OptionalRouter.get('/', authenticateToken, authorizeRole(['admin', 'user']), (req, res) => optionalController.getAllOptionalItems(req, res)); // GET /optional-items
+OptionalRouter.post('/', authenticateToken, authorizeRole(['admin']), (req, res) => optionalController.createOptionalItem(req, res)); // POST /optional-items
+OptionalRouter.get('/:id', authenticateToken, authorizeRole(['admin', 'user']), (req, res) => optionalController.getOptionalItemById(req, res)); // GET /optional-items/:id
+OptionalRouter.put('/:id', authenticateToken, authorizeRole(['admin']), (req, res) => optionalController.updateOptionalItemById(req, res)); // PUT /optional-items/:id
+OptionalRouter.delete('/:id', authenticateToken, authorizeRole(['admin']), (req, res) => optionalController.deleteOptionalItemById(req, res)); // DELETE /optional-items/:id
 
 export default OptionalRouter;
